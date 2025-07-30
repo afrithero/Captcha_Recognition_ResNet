c97ebf01ce562cf61698d081588ada492088855d
afrithero <afrithero@gmail.com>
2025-07-30 12:57:57 +0800

    Remove testing code

--- src/model_config.py
+++ src/model_config.py
@@ -1,6 +1,6 @@
 import torch
 
-DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu") # Linux
 SAVE_FREQ = 10
 EPOCHS = 100
 BATCH_SIZE = 100
