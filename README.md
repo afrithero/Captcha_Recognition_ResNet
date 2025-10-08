e6c0c4413aa226f5ddcbf0d491ff18da450e1e4f
Aaron Chang <afrithero@gmail.com>
2025-10-09 00:33:16 +0800

    add mlflow

--- README.md
+++ README.md
@@ -4,17 +4,17 @@ This repository implements a deep learning-based Captcha recognition system usin
 
 The current implementation supports inference on Captcha images, and more details (including training pipeline, dataset information) will be added soon.
 # Problem Description
-The Captcha images contain a sequence of digits, and the model must learn not only to recognize individual digits but also to preserve their order, which is essential for correct interpretation.
+The Captcha images contain a sequence of characters, and the model must learn not only to recognize individual characters but also to preserve their order, which is essential for correct interpretation.
 
 We define three progressively challenging tasks:
 
-- Task 1: Recognize a single digit in the image.
+- Task 1: Recognize a single character in the image.
 
-- Task 2: Recognize two digits in the image, where the order of digits matters.
+- Task 2: Recognize two characters in the image, where the order of characters matters.
 
-- Task 3: Recognize four digits in the image, maintaining the correct sequential order.
+- Task 3: Recognize four characters in the image, maintaining the correct sequential order.
 
-An example is shown below, where the target output is '0751'. Each digit is highlighted and labeled to indicate its position in the sequence. The model must output the correct digit string by identifying the characters and their order from left to right.
+An example is shown below, where the target output is '0751'. Each character is highlighted and labeled to indicate its position in the sequence. The model must output the correct string by identifying the characters and their order from left to right.
 
 ![Logo](https://i.postimg.cc/fy9TB6cr/2025-07-30-3-56-10.png)
 
@@ -44,8 +44,47 @@ The directory named dataset should be placed under the root level of this reposi
 
 This project includes a reproducible preprocessing pipeline to (1) **augment** the training images and (2) **denoise** them for more robust training. The script is `src/data_preprocess.py`. It operates on three tasks stored under `dataset/train/{task1,task2,task3}`.
 ```bash
+cd src
 python data_preprocess.py
 ```
 
-## Coming Soon
-- How to train and evaluate.
+## Train the Model
+```bash
+cd src
+python main.py --mode train
+```
+
+## Evaluate the Model
+- Using a local checkpoint
+```bash
+cd src
+python main.py --mode evaluate --checkpoint [your checkpoint path]
+```
+- Using a registered MLflow model
+```bash
+cd src
+python main.py --mode evaluate --uri [your registered MLflow model uri]
+```
+
+## Predict
+- Using a local checkpoint
+```bash
+cd src
+python main.py --mode predict \
+  --checkpoint [your checkpoint path] \
+  --sample_submission [input csv path] \
+  --submission [output csv path]
+
+```
+
+- Using a registered MLflow model
+```bash
+cd src
+python main.py --mode predict \
+  --uri [your registered MLflow model uri] \
+  --sample_submission [input csv path] \
+  --submission [output csv path]
+
+```
+
+
--- src/trainer.py
+++ src/trainer.py
@@ -9,32 +9,36 @@ from model import ResNet, ResidualBlock
 import data_encoding
 import numpy as np
 import pandas as pd
+import mlflow
+import mlflow.pytorch
 
 class Trainer:
-    def __init__(self, model_cls, device, batch_size, epochs, 
+    def __init__(self, model, device, batch_size, epochs, 
                 save_dir, lr, val_batch_size=1, val_shuffle=False, val_drop_last=False):
 
         self.device = device
         self.epochs = epochs
         self.save_dir = save_dir
+        self.batch_size = batch_size
+        self.lr = lr
         
         os.makedirs(save_dir, exist_ok=True)
         
-        self.model = model_cls(ResidualBlock).to(device)       
+        self.model = model(ResidualBlock).to(device)       
 
         if torch.cuda.device_count() > 1:
             self.model = torch.nn.DataParallel(self.model)
          
-        self.optimizer = torch.optim.Adam(self.model.parameters(), lr=lr)
+        self.optimizer = torch.optim.Adam(self.model.parameters(), lr=self.lr)
         self.criterion = torch.nn.MultiLabelSoftMarginLoss()
 
-        # 讀並切訓練 / 驗證資料
+        # split data and parse annotation
         df = parse_annotation(data_config.ROOT_PATH)
         df_train, df_val = split_train_val(df, random_seed=42)
 
         self.train_loader = DataLoader(
             CaptchaDataset(data_config.ROOT_PATH, df_train),
-            batch_size=batch_size, shuffle=True,
+            batch_size=self.batch_size, shuffle=True,
             num_workers=4, drop_last=True)
         
         self.val_loader = DataLoader(
@@ -43,18 +47,30 @@ class Trainer:
             num_workers=4, drop_last=val_drop_last)
 
     def train(self):
-        for epoch in range(self.epochs):
-            total_loss = self._train_one_epoch(epoch)
-            
-            print(f"[Epoch {epoch+1}] total loss: {total_loss:.4f}")
+        with mlflow.start_run(run_name="train") as run:
+            mlflow.log_param("batch_size", self.batch_size)
+            mlflow.log_param("epochs", self.epochs)
+            mlflow.log_param("learning_rate", self.lr)
 
-            if (epoch + 1) % model_config.SAVE_FREQ == 0:
-                path = os.path.join(
-                    self.save_dir,
-                    f"resnet_epoch_{epoch+1}.pkl")
+            for epoch in range(self.epochs):
+                total_loss = self._train_one_epoch(epoch)
+                mlflow.log_metric("train_loss", total_loss, step=epoch)
 
-                torch.save(self.model.state_dict(), path)
-                print(f"Checkpoint saved to {path}")
+                if (epoch + 1) % model_config.SAVE_FREQ == 0:
+                    checkpoint_path = os.path.join(
+                        self.save_dir,
+                        f"resnet_epoch_{epoch+1}.pkl")
+
+                    print(f'saving to {checkpoint_path}')
+                    torch.save(self.model.state_dict(), checkpoint_path)
+                    mlflow.log_artifact(checkpoint_path, artifact_path="checkpoints")
+
+            mlflow.pytorch.log_model(self.model, artifact_path="resnet_model")
+            result = mlflow.register_model(
+                f"runs:/{run.info.run_id}/resnet_model",
+                "CaptchaResNet"
+            )              
+                        
 
     def _train_one_epoch(self, epoch):
         self.model.train()
@@ -74,46 +90,53 @@ class Trainer:
                 total_loss += loss.item()
                 batches.set_postfix(loss=loss.item())
         
-        return total_loss
-
-    def evaluate(self, checkpoint_path=None):
+        return total_loss / len(self.train_loader)
+    
+    def _load_model(self, checkpoint_path=None, model_uri=None):
         if checkpoint_path:
             self.model.load_state_dict(torch.load(checkpoint_path))
-            print(f"Loaded weights from {checkpoint_path}")
-        
-        self.model.eval()
+            self.model.to(self.device)
+        elif model_uri:
+            self.model = mlflow.pytorch.load_model(model_uri).to(self.device)
+        else:
+            raise ValueError("Please provide either checkpoint_path or model_uri.")
 
-        correct = 0
-        total   = 0
-        batches = tqdm(self.val_loader, desc="Evaluate", leave=False)
+    def evaluate(self, checkpoint_path=None, model_uri=None):
+        with mlflow.start_run(run_name="evaluate"):
+            self._load_model(checkpoint_path, model_uri)
+            self.model.eval()
 
-        with torch.no_grad():
-            for images, labels, fnames in batches:
-                images = images.to(self.device)
-                preds  = self.model(images).cpu().numpy()
-                true   = data_encoding.decode(labels.numpy()[0])
+            correct = 0
+            total   = 0
+            batches = tqdm(self.val_loader, desc="Evaluate", leave=False)
 
-                # slicing + argmax to rebuild pred_str
-                ALL = data_config.ALL_CHAR_SET_LEN
-                chars = []
-                
-                for i in range(len(true)):
-                    start = i*ALL
-                    end = (i+1)*ALL
-                    idx = np.argmax(preds[0, start:end])
-                    chars.append(data_config.INDEX_TO_CAPTCHA_DICT[idx])
-                
-                pred_str = ''.join(chars)
+            with torch.no_grad():
+                for images, labels, fnames in batches:
+                    images = images.to(self.device)
+                    preds  = self.model(images).cpu().numpy()
+                    true   = data_encoding.decode(labels.numpy()[0])
 
-                if pred_str == true:
-                    correct += 1
-                total += 1
+                    # slicing + argmax to rebuild pred_str
+                    ALL = data_config.ALL_CHAR_SET_LEN
+                    chars = []
+                    
+                    for i in range(len(true)):
+                        start = i*ALL
+                        end = (i+1)*ALL
+                        idx = np.argmax(preds[0, start:end])
+                        chars.append(data_config.INDEX_TO_CAPTCHA_DICT[idx])
+                    
+                    pred_str = ''.join(chars)
 
-                batches.set_description(f"[T:{true}|P:{pred_str}]")
-                tqdm.write(f"{fnames[0]} → True:{true}, Pred:{pred_str}")
+                    if pred_str == true:
+                        correct += 1
+                    total += 1
 
-        acc = correct / total if total>0 else 0
-        print(f"Evaluation result: {correct}/{total} = {acc:.4f}")
+                    batches.set_description(f"[T:{true}|P:{pred_str}]")
+                    tqdm.write(f"{fnames[0]} → True:{true}, Pred:{pred_str}")
+
+            acc = correct / total if total > 0 else 0
+            mlflow.log_metric("eval_accuracy", acc)
         
         return acc
 
@@ -130,42 +153,42 @@ class Trainer:
         return df
 
     def predict(self, sample_submission_path, submission_path, checkpoint_path=None):
-        if checkpoint_path:
-            self.model.load_state_dict(torch.load(checkpoint_path))
-            print(f"Loaded weights from {checkpoint_path}")
-            
-        self.model.eval()
+        with mlflow.start_run(run_name="predict"):
+            self._load_model(checkpoint_path, model_uri)
 
-        df_test = self._parse_submission(sample_submission_path)
-        test_ds = CaptchaDataset(data_config.ROOT_PATH, df_test, is_predict=True)
-        test_dl = DataLoader(
-            test_ds,
-            batch_size=1,
-            shuffle=False,
-            num_workers=4,
-            drop_last=False
-        )
+            self.model.eval()
 
-        preds = []
-        batches = tqdm(test_dl, total=len(test_dl), desc="Predict", leave=False)
-        with torch.no_grad():
-            for images, labels, fnames in batches:
-                images = images.to(self.device)
-                pred_probs = self.model(images).cpu().numpy()
-                length = int(labels.numpy()[0])
+            df_test = self._parse_submission(sample_submission_path)
+            test_ds = CaptchaDataset(data_config.ROOT_PATH, df_test, is_predict=True)
+            test_dl = DataLoader(
+                test_ds,
+                batch_size=1,
+                shuffle=False,
+                num_workers=4,
+                drop_last=False
+            )
 
-                ALL = data_config.ALL_CHAR_SET_LEN
-                chars = []
-                for i in range(length):
-                    start, end = i*ALL, (i+1)*ALL
-                    idx = np.argmax(pred_probs[0, start:end])
-                    chars.append(data_config.INDEX_TO_CAPTCHA_DICT[idx])
-                pred_str = ''.join(chars)
-                preds.append(pred_str)
+            preds = []
+            batches = tqdm(test_dl, total=len(test_dl), desc="Predict", leave=False)
+            with torch.no_grad():
+                for images, labels, fnames in batches:
+                    images = images.to(self.device)
+                    pred_probs = self.model(images).cpu().numpy()
+                    length = int(labels.numpy()[0])
 
-                batches.set_description(f"[len:{length}|pred:{pred_str}]")
-                batches.set_postfix(file=fnames)
+                    ALL = data_config.ALL_CHAR_SET_LEN
+                    chars = []
+                    for i in range(length):
+                        start, end = i*ALL, (i+1)*ALL
+                        idx = np.argmax(pred_probs[0, start:end])
+                        chars.append(data_config.INDEX_TO_CAPTCHA_DICT[idx])
+                    pred_str = ''.join(chars)
+                    preds.append(pred_str)
 
-        df_test['label'] = preds
-        df_test.to_csv(submission_path, index=False)
-        print(f"Submission saved to {submission_path}")
+                    batches.set_description(f"[len:{length}|pred:{pred_str}]")
+                    batches.set_postfix(file=fnames)
+
+            df_test['label'] = preds
+            df_test.to_csv(submission_path, index=False)
+            mlflow.log_artifact(submission_path)
+            print(f"Submission saved to {submission_path}")
